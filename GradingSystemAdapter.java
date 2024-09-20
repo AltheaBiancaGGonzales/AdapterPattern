feb55658037e13a6fd9c839cf14e4248e419c666
Althea Gonzales <altheagonzalesctr@gmail.com>
2024-09-20 10:54:55 +0800

    Update GradingSystemAdapter.java

--- GradingSystemAdapter.java
+++ GradingSystemAdapter.java
@@ -1,1 +1,14 @@
+public class GradingSystemAdapter implements SchoolManagementApp {
+    private GradingSystem gradingSystem;
 
+    public GradingSystemAdapter(GradingSystem gradingSystem) {
+        this.gradingSystem = gradingSystem;
+    }
+
+    @Override
+    public void integrateSystem(String... args) {
+        if (args.length > 1) {
+            gradingSystem.recordGrades(args[0], args[1]);
+        }
+    }
+}
