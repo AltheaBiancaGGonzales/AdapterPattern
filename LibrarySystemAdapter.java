75c91139684d9469932b7fb67c1f568ac882cee3
Althea Gonzales <altheagonzalesctr@gmail.com>
2024-09-20 10:55:18 +0800

    Update LibrarySystemAdapter.java

--- LibrarySystemAdapter.java
+++ LibrarySystemAdapter.java
@@ -1,1 +1,14 @@
+public class LibrarySystemAdapter implements SchoolManagementApp {
+    private LibrarySystem librarySystem;
 
+    public LibrarySystemAdapter(LibrarySystem librarySystem) {
+        this.librarySystem = librarySystem;
+    }
+
+    @Override
+    public void integrateSystem(String... args) {
+        if (args.length > 1) {
+            librarySystem.manageBooks(args[0], args[1]);
+        }
+    }
+}
