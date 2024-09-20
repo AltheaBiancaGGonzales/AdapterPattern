e14fda2beb4821d75c33ece09f0aafec7998082c
Althea Gonzales <altheagonzalesctr@gmail.com>
2024-09-20 10:58:59 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,19 @@
 
+public class Main {
+   public static void main(String[] args) {
+       
+       AttendanceSystem attendanceSystem = new AttendanceSystem();
+       GradingSystem gradingSystem = new GradingSystem();
+       LibrarySystem librarySystem = new LibrarySystem();
+
+      
+       SchoolManagementApp attendanceAdapter = new AttendanceSystemAdapter(attendanceSystem);
+       SchoolManagementApp gradingAdapter = new GradingSystemAdapter(gradingSystem);
+       SchoolManagementApp libraryAdapter = new LibrarySystemAdapter(librarySystem);
+
+       
+       attendanceAdapter.integrateSystem("22724");
+       gradingAdapter.integrateSystem("13139", "A");
+       libraryAdapter.integrateSystem("Book_12", "add");
+   }
+}
