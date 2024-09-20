2c5cb8f6c111e820b1e94b871f9ee52ea0ba223f
Althea Gonzales <altheagonzalesctr@gmail.com>
2024-09-20 10:53:41 +0800

    Update LibrarySystem.java

--- LibrarySystem.java
+++ LibrarySystem.java
@@ -1,1 +1,5 @@
-
+public class LibrarySystem {
+    public void manageBooks(String bookId, String action) {
+        System.out.println("Action '" + action + "' performed on book: " + bookId);
+    }
+}
