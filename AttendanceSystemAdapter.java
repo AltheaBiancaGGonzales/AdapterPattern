3338e548baa1be6b2a7bc2c299347056eb03381d
Althea Gonzales <altheagonzalesctr@gmail.com>
2024-09-20 10:54:31 +0800

    Update AttendanceSystemAdapter.java

--- AttendanceSystemAdapter.java
+++ AttendanceSystemAdapter.java
@@ -1,1 +1,15 @@
 
+public class AttendanceSystemAdapter implements SchoolManagementApp {
+    private AttendanceSystem attendanceSystem;
+
+    public AttendanceSystemAdapter(AttendanceSystem attendanceSystem) {
+        this.attendanceSystem = attendanceSystem;
+    }
+
+    @Override
+    public void integrateSystem(String... args) {
+        if (args.length > 0) {
+            attendanceSystem.markAttendance(args[0]);
+        }
+    }
+}
